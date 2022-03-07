85dbca34ff657c17d177500b155a59937e40437f
WILLIAM STEINBRINK <william.steinbrink@gmail.com>
2022-03-07 18:29:18 -0500

    added PORT var

--- http_server.js
+++ http_server.js
@@ -7,6 +7,7 @@ var low     = require('lowdb');
 var fs      = require('lowdb/adapters/FileSync');
 var adapter = new fs('db.json');
 var db      = low(adapter);
+const PORT = process.env.PORT || 5000
 
 // init the data store
 db.defaults({ users: []}).write();
